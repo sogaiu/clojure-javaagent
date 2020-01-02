e702399733773c08ff6418850020ab7c932b6620
sogaiu <983021772@users.noreply.github.com>
2020-01-03 01:35:23 +0900

    Reduce use of reflection

--- src/clojure_javaagent/core.clj
+++ src/clojure_javaagent/core.clj
@@ -4,11 +4,13 @@
   (:import
    [com.sun.tools.attach VirtualMachine]))
 
+(set! *warn-on-reflection* true)
+
 (defn -main
   [& args]
   ;; pid - string
   ;; port - integer as string
-  (let [[pid port] args
+  (let [[^String pid port] args
         project-path (.getCanonicalPath (cji/file "."))
         javaagent-path (str project-path "/target/clojure-javaagent-1.0.0.jar")]
     (.loadAgent (VirtualMachine/attach pid) javaagent-path (str port))))
